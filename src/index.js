fd73e4bbe6f9dd2c1785e64b84a3c7d26cd08e57
Jason Hennig <jasonhennig@jasons-mbp.myfiosgateway.com>
2024-05-23 17:10:48 -0400

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ProductFetcher from './Components/ProductFetcher';
